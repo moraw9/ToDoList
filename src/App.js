44944418332c568c72a9d7465077499ad8f36bd0
Daniel Szczepanik <tmlm18@gmail.com>
2021-07-18 21:55:15 +0200

    Add initial todos data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@
 // We will go through slides and build the app together during the
 // React intro workshops at Selleo.
 
+const todos = [
+  { title: "Learn ReactJS", isDone: false, isArchived: false },
+  { title: "Attend ReactJS workshops", isDone: true, isArchived: false },
+  { title: "Learn Ruby on Rails", isDone: true, isArchived: false },
+  { title: "Attend Ruby on Rails workshops", isDone: true, isArchived: false },
+  {
+    title: "This one shouldn't be visible - archived",
+    isDone: true,
+    isArchived: true,
+  },
+];
+
 const App = () => {
   return (
     <div className="main__wrapper">
