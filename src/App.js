009a5898e54671a1120b081a72274ffc680f3b88
Daniel Szczepanik <tmlm18@gmail.com>
2021-07-18 21:22:33 +0200

    Setup Sass variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// Hi!
+
+// We'll be creating a "Todo App", which is a well-known app type
+// for learning basic concepts of web/mobile frameworks.
+
+// We will go through slides and build the app together during the
+// React intro workshops at Selleo.
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="main__wrapper">
+      <h1 className="main__title">👋 Hello from Selleo</h1>
+    </div>
+  );
 };
 
 export default App;
